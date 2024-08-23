f3dc2a05fa3e0176e7f9044f27e8ba731381c009
PraveenPraveenVenkat <praveenvenkatachalam2001@gmail.com>
2024-08-23 22:59:20 +0530

    Square-Patten

---
+++ square.java
@@ -0,0 +1,13 @@
+public class square {
+    public static void main(String[] args) 
+    {
+        // System.out.println("Hello World");
+        for (int r=0;r<5;r++)
+        {
+   for(int c=0;c<5;c++)
+   {
+   System.out.println("*");
+   }
+        }
+    }
+}
