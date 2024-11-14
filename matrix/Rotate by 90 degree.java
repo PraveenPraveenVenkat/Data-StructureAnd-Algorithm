3f5a5644941c0f3b5bc45d7650b78c04b9140c11
praveen <praveenvenkatachalam2001@gmail.com>
2024-11-14 22:19:05 +0530

    Started Martix

--- matrix/Rotate by 90 degree.java
+++ matrix/Rotate by 90 degree.java
@@ -1,40 +1,22 @@
-
-// User function Template for Java
-
 class Solution {
-    // Function for finding maximum and value pair
-    public static int lenOfLongSubarr(int A[], int N, int K) {
-        // Complete the function
-        
-          // HashMap to store (prefix_sum, first_occurrence_index)
-        HashMap<Integer, Integer> sum_map = new HashMap<>();
-        int current_sum = 0; // This will store the running prefix sum
-        int max_length = 0;  // To store the maximum length of sub-array with sum equal to K
-        
-        // Iterate over the array
-        for (int i = 0; i < N; i++) {
-            // Add the current element to the running sum
-            current_sum += A[i];
-            
-            // Case 1: Check if the sum from the start to the current index equals K
-            if (current_sum == K) {
-                max_length = i + 1; // The whole array up to i is the sub-array
-            }
-            
-            // Case 2: Check if (current_sum - K) exists in the map
-            if (sum_map.containsKey(current_sum - K)) {
-                // Calculate the length of the sub-array
-                int length = i - sum_map.get(current_sum - K);
-                max_length = Math.max(max_length, length);
-            }
-            
-            // Case 3: Store the current_sum in the map if it hasn't been stored already
-            if (!sum_map.containsKey(current_sum)) {
-                sum_map.put(current_sum, i);
-            }
-        }
-        
-        // Return the maximum length of the sub-array with sum K
-        return max_length;
-    }
-}
+    // Function to rotate matrix anticlockwise by 90 degrees.
+    static void rotateby90(int matrix[][]) {
+        // code here
+         int temp=0;
+        int n=matrix[0].length;
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length/2;j++){
+                    temp=matrix[i][j];
+                    matrix[i][j]=matrix[i][n-j-1];
+                     matrix[i][n-j-1]=temp;
+                     
+                 }
+             }
+                   for(int i=0;i<matrix.length;i++){
+                 for(int j=i;j<matrix[0].length;j++){
+                     temp=matrix[i][j];
+                     matrix[i][j]=matrix[j][i];
+                     matrix[j][i]=temp;
+                  }
+          }
+      }
