7f2328c01201d7c3a338292238d7cd857e43cf29
PraveenPraveenVenkat <praveenvenkatachalam2001@gmail.com>
2025-09-23 12:49:10 +0530

    feat:Reverse an Arrays

---
+++ Array/Arrays/Reverse an Array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void reverseArray(int arr[]) {
+        // code here
+        //this for loop only for reverse
+        // for(int i=arr.length-1;i>=0;i--){
+        //     System.out.print(arr[i]);
+
+        // the while Condition for reverse index
+         int start = 0;
+         int end = arr.length-1;
+        while(start<=end){
+            int temp =arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            
+           start++;
+           end--;
+            
+          }
+       }
+   }
