a9e0d2eaf0e155f5076da5cbc00a245df8916ba2
PraveenPraveenVenkat <praveenvenkatachalam2001@gmail.com>
2025-09-23 12:52:33 +0530

    feat:Sort an Arrays

---
+++ DSA/Arrays/Sort an Array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+            Arrays.sort(nums);
+            return nums;
+        }
+
+    }
+
+
+// class Solution {
+//     public int[] sortArray(int[] nums) {
+//         // Simple bubble sort
+//         for (int i = 0; i < nums.length - 1; i++) {
+//             for (int j = 0; j < nums.length - i - 1; j++) {
+//                 if (nums[j] > nums[j + 1]) {
+//                     int temp = nums[j];
+//                     nums[j] = nums[j + 1];
+//                     nums[j + 1] = temp;
+//                 }
+//             }
+//         }
+//         return nums;
+//     }
+// }
