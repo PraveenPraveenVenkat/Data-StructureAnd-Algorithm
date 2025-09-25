d1628fb6256e61be3d1cbeed426266a102cc9247
PraveenPraveenVenkat <praveenvenkatachalam2001@gmail.com>
2025-09-25 21:38:02 +0530

    feat:Product of Array Except Self in Arrays

---
+++ DSA/Arrays/Product of Array Except Self.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+            
+            int n =nums.length;
+            int [] res = new  int[n];
+            if(n==0) return res;
+
+            int[] leftProduct = new int[n];
+            int[] rightProduct = new  int[n];
+            
+
+            int product =1;
+            for(int i=0; i<n; i++){
+                product *=nums[i];
+                leftProduct[i] = product;
+            }
+
+            product = 1;
+            for(int i=n-1 ; i>=0 ; i--){
+                product *=nums[i];
+                rightProduct[i] = product;
+            }
+          
+          res[0] =rightProduct[1];
+          res[n-1] = leftProduct[n-2];
+
+           for(int i=1; i<n-1; i++){
+            res[i] = leftProduct[i - 1] * rightProduct[i + 1]; 
+           }
+           return res;
+    }
+}
