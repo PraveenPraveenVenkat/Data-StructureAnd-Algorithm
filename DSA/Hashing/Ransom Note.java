b47c2c1dcff83a6a70e226433f58ab75442497d3
PraveenPraveenVenkat <praveenvenkatachalam2001@gmail.com>
2025-10-14 22:38:49 +0530

    feat:Insert Delete Get Random  in Hashing

--- DSA/Hashing/Ransom Note.java
+++ DSA/Hashing/Ransom Note.java
@@ -1,3 +1,4 @@
+//! leetCode383
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
 		if (ransomNote.length() > magazine.length()) return false;
@@ -13,3 +14,51 @@ class Solution {
         return true;
     }
 }
+
+
+//! Hareesh approach
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+		
+        int hash[] = new int[128];
+        for(char i:magazine.toCharArray()){
+            hash[i]++;
+        }
+           for(char i:ransomNote.toCharArray()){
+            hash[i]--;
+        }
+        for(int i:hash){
+            if(i<0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+//! Another approach
+
+import java.util.HashMap;
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        // Step 1: Count letters in magazine
+        for (char c : magazine.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        // Step 2: Check ransomNote letters
+        for (char c : ransomNote.toCharArray()) {
+            if (!map.containsKey(c) || map.get(c) == 0) {
+                return false; // not enough of this letter
+            }
+            map.put(c, map.get(c) - 1);
+        }
+
+        // Step 3: All letters are available
+        return true;
+    }
+}
+
