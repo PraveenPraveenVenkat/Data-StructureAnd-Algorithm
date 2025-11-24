479ed3057c036b81449e481da0d4ac48094a7613
PraveenPraveenVenkat <praveenvenkatachalam2001@gmail.com>
2025-11-24 22:17:05 +0530

    feat:permute in  Recursion

---
+++ DSA/BackTracking/Permutations.java
@@ -0,0 +1,68 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        List<Integer> temp = new ArrayList<>();
+
+        backtracking(nums, used, temp, res);
+        return res;
+    }
+
+    private void backtracking(int[] nums, boolean[] used, List<Integer> temp, List<List<Integer>> res) {
+
+        // Base case: if permutation is complete
+        if (temp.size() == nums.length) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;    // already used in this permutation
+
+            // choose
+            used[i] = true;
+            temp.add(nums[i]);
+
+            // explore
+            backtracking(nums, used, temp, res);
+
+            // un-choose
+            used[i] = false;
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
+
+// class Solution {
+//     public List<List<Integer>> permute(int[] nums) {
+        
+//         List<List<Integer> res= new ArrayList<>();
+
+//         backtracking(res,nums,);
+//     }
+//     private void backtracking(List<List<Integer> res,int used,int nums){
+
+       
+       
+//             if(temp.size()==nums.length){
+//                 res.add(new ArrayList<>(temp));
+//                 return;
+            
+
+//              for(int i=0;i<nums.length;i++){
+//               if(used[i]) continue;
+//               used[i] =true;
+//               temp.add(nums[i]);
+//               backtracking(res,nums,);
+//               temp.remove(nums[i]);
+//               used[i] =false;
+
+//             }
+           
+
+            
+//         }
+
+        
+//     }
+// }
